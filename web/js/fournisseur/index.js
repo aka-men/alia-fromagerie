b371973c3805b16588dd7a54f2a880e71a1f33ea
abdelhak <abdelhak.ouaddi@gmail.com>
2017-04-11 01:55:11 +0000

    Instalation de base

---
+++ web/js/fournisseur/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by abdelhak on 10/04/2017.
+ */
+
+$(function(){
+
+    $('#appbundle_fournisseur').validate({
+        highlight: function (input) {
+            $(input).parents('.form-line').addClass('error');
+        },
+        unhighlight: function (input) {
+            $(input).parents('.form-line').removeClass('error');
+        },
+        errorPlacement: function (error, element) {
+            $(element).parents('.form-group').append(error);
+        }
+    });
+
+});
